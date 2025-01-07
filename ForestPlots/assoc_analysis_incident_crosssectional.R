9a968f1115c1ef720cc890070a5b9fb878accf83
Yana Hrytsenko <39499301+YanaHrytsenko@users.noreply.github.com>
2025-01-07 11:50:56 -0500

    updated outputs

--- ForestPlots/assoc_analysis_incident_crosssectional.R
+++ ForestPlots/assoc_analysis_incident_crosssectional.R
@@ -6,156 +6,8 @@ library(dplyr)
 library(wesanderson)
 library(stringr)
 library(ggpubr)
-# #read in results for T2D PRS assoc w DM at baseline
-# PRS_vec <- c("PRSstd_mgb_std","PRSstd_sum_std", "PRSstd_gap_std") 
-# 
-# res1 <- read.csv("/Users/yana/Library/CloudStorage/OneDrive-BethIsraelLaheyHealth/2022_diabetes_PRS_interaction_sleep/Code/Code_for_GitHub/Results/assoc_PRS_baseline_DM_w_comparison_PRSs.csv")
-# 
-# res1 <- res1 %>% filter(PRS %in% PRS_vec)
-# 
-# res1$OR <- exp(res1$log_OR)
-# res1$CI_low <- exp(res1$log_OR -1.96*res1$log_OR_SE)
-# res1$CI_high <- exp(res1$log_OR +1.96*res1$log_OR_SE)
-# 
-# res1$PRS <- as.factor(res1$PRS)
-# res1$X <- c(1:nrow(res1))
-# 
-# # subset by OSA categories only 
-# OSA_strats <- c("All","No_OSA", "Mild_OSA", "Mod_severe_OSA")
-# res1_OSA <- res1 %>% filter(stratum %in% OSA_strats)
-# 
-# #rename variables to not include underscores
-# res1_OSA <- res1_OSA %>% mutate(stratum=recode(stratum, 'All'= 'Overall','No_OSA'='No OSA', 'Mild_OSA' = 'Mild OSA', 'Mod_severe_OSA' = 'Moderate to severe OSA'))
-# 
-# #rename PRSs
-# res1_OSA <- res1_OSA %>% mutate(PRS=recode(PRS, 'PRSstd_sum_std'= 'PRSsum','PRSstd_gap_std'='gapPRSsum', 'PRSstd_mgb_std' = 'mgbPRSsum', 'PGS002308_std_PRSs' = 'PGS002308_PRS', 'PGS003867_std_PRSs' = 'PGS003867_PRS'))
-# 
-# #format ORs
-# res1_OSA$OR_CI <- paste0(format(round(res1_OSA$OR, 2), nsmall = 2), " [", format(round(res1_OSA$CI_low, 2), nsmall = 2), " ; ", format(round(res1_OSA$CI_high, 2), nsmall = 2), "]")
-# 
-# ## add AUCs
-# assoc_PRS_baseline <- read.csv("/Users/yana/Library/CloudStorage/OneDrive-BethIsraelLaheyHealth/2022_diabetes_PRS_interaction_sleep/Code/Code_for_GitHub/Results/assoc_PRS_baseline_DM_w_AUC_CI.csv")
-# 
-# assoc_PRS_baseline <- assoc_PRS_baseline[which(assoc_PRS_baseline$PRS %in% PRS_vec),]
-# assoc_PRS_baseline$AUC_CI <- paste0(format(round(assoc_PRS_baseline$mean_AUC, 2), nsmall = 2), " [", format(round(assoc_PRS_baseline$AUC_CI_lower, 2), nsmall = 2), " ; ", format(round(assoc_PRS_baseline$AUC_CI_upper, 2), nsmall = 2), "]")
-# assoc_PRS_baseline <- assoc_PRS_baseline %>% mutate(PRS=recode(PRS, 'PRSstd_sum_std'= 'PRSsum','PRSstd_gap_std'='gapPRSsum', 'PRSstd_mgb_std' = 'mgbPRSsum'))
-# 
-# #extract only sleep phenos we need
-# assoc_PRS_baseline_OSA <- assoc_PRS_baseline %>% filter(stratum %in% OSA_strats)
-# 
-# assoc_PRS_baseline_OSA <- assoc_PRS_baseline_OSA %>% mutate(stratum=recode(stratum, 'All'= 'Overall','No_OSA'='No OSA', 'Mild_OSA' = 'Mild OSA', 'Mod_severe_OSA' = 'Moderate to severe OSA'))
-# 
-# #extract only AUC and the PRS type to merge on
-# assoc_PRS_baseline_OSA_sub <- assoc_PRS_baseline_OSA[,c("stratum", "PRS","mean_AUC","AUC_CI_lower", "AUC_CI_upper","AUC_CI")]
-# 
-# res1_OSA <- merge(res1_OSA, assoc_PRS_baseline_OSA_sub, by.x=c("stratum", "PRS"))
-# 
-# res1_OSA <- rbind(res1_OSA[10:12,],res1_OSA[7:9,], res1_OSA[1:3,], res1_OSA[4:6,])
-# res1_OSA$X <- c(nrow(res1_OSA):1)
-# 
-# res1_OSA$log_OR_p <- signif(res1_OSA$log_OR_p, digits=1)
-# 
-# #only plot mgbPRS
-# #res1_OSA <- res1_OSA[which(res1_OSA$PRS == "mgbPRSsum"),]
-# 
-# #res1_OSA$PRS <- factor(res1_OSA$PRS, levels = c("mgbPRSsum"))
-# 
-# res1_OSA$stratum <- factor(res1_OSA$stratum, levels = c("Overall","No OSA", "Mild OSA", "Moderate to severe OSA"))
-# 
-# #only keep the columns we need
-# res1_OSA <- res1_OSA[c("X", "n", "N_case", "N_control","stratum", "PRS","OR", "CI_low", "CI_high", "log_OR_p", "OR_CI", "mean_AUC","AUC_CI_lower", "AUC_CI_upper", "AUC_CI")]
-# 
-# res1_OSA$Analysis <- "Cross-sectional"
-# 
-# res1_OSA$X <- c(8,7,6,5)
-# 
-# 
-# #add incident results
-# PRS_vec <- c("PRSstd_mgb_std","PRSstd_sum_std", "PRSstd_gap_std")
-# res2 <- read.csv("/Users/yana/Library/CloudStorage/OneDrive-BethIsraelLaheyHealth/2022_diabetes_PRS_interaction_sleep/Code/Code_for_GitHub/Results/assoc_PRS_incident_DM_w_comparison_PRSs.csv")
-# 
-# res2 <- res2 %>% filter(PRS %in% PRS_vec)
-# 
-# res2$IRR <- exp(res2$log_IRR)
-# res2$CI_low <- exp(res2$log_IRR -1.96*res2$log_IRR_SE)
-# res2$CI_high <- exp(res2$log_IRR +1.96*res2$log_IRR_SE) 
-# 
-# res2$PRS <- as.factor(res2$PRS)
-# res2$X <- c(1:nrow(res2))
-# 
-# # subset by OSA categories only 
-# OSA_strats_2 <- c("All","No_OSA", "Mild_OSA", "Mod_severe_OSA")
-# res2_OSA <- res2 %>% filter(stratum %in% OSA_strats_2)
-# 
-# 
-# #rename variables to not include underscores
-# res2_OSA <- res2_OSA %>% mutate(stratum=recode(stratum, 'All'= 'Overall','No_OSA'='No OSA', 'Mild_OSA' = 'Mild OSA', 'Mod_severe_OSA' = 'Moderate to severe OSA'))
-# 
-# #rename PRSs
-# res2_OSA <- res2_OSA %>% mutate(PRS=recode(PRS, 'PRSstd_sum_std'= 'PRSsum','PRSstd_gap_std'='gapPRSsum', 'PRSstd_mgb_std' = 'mgbPRSsum', 'PGS002308_std_PRSs' = 'PGS002308_PRS', 'PGS003867_std_PRSs' = 'PGS003867_PRS'))
-# 
-# 
-# res2_OSA$IRR_CI <- paste0(format(round(res2_OSA$IRR, 2), nsmall = 2), " [", format(round(res2_OSA$CI_low, 2), nsmall = 2), " ; ", format(round(res2_OSA$CI_high, 2), nsmall = 2), "]")
-# 
-# 
-# ## add AUCs
-# assoc_PRS_incident <- read.csv("/Users/yana/Library/CloudStorage/OneDrive-BethIsraelLaheyHealth/2022_diabetes_PRS_interaction_sleep/Code/Code_for_GitHub/Results/assoc_PRS_incident_DM_w_AUC_CI.csv")
-# 
-# assoc_PRS_incident <- assoc_PRS_incident[which(assoc_PRS_incident$PRS %in% PRS_vec),]
-# assoc_PRS_incident$AUC_CI <- paste0(format(round(assoc_PRS_incident$mean_AUC, 2), nsmall = 2), " [", format(round(assoc_PRS_incident$AUC_CI_lower, 2), nsmall = 2), " ; ", format(round(assoc_PRS_incident$AUC_CI_upper, 2), nsmall = 2), "]")
-# assoc_PRS_incident <- assoc_PRS_incident %>% mutate(PRS=recode(PRS, 'PRSstd_sum_std'= 'PRSsum','PRSstd_gap_std'='gapPRSsum', 'PRSstd_mgb_std' = 'mgbPRSsum'))
-# 
-# #extract only sleep phenos we need
-# assoc_PRS_incident_OSA <- assoc_PRS_incident %>% filter(stratum %in% OSA_strats_2)
-# 
-# assoc_PRS_incident_OSA <- assoc_PRS_incident_OSA %>% mutate(stratum=recode(stratum, 'All'= 'Overall','No_OSA'='No OSA', 'Mild_OSA' = 'Mild OSA', 'Mod_severe_OSA' = 'Moderate to severe OSA'))
-# 
-# #extract only AUC and the PRS type to merge on
-# assoc_PRS_incident_OSA_sub <- assoc_PRS_incident_OSA[,c("stratum", "PRS","mean_AUC","AUC_CI_lower", "AUC_CI_upper", "AUC_CI")]
-# 
-# res2_OSA <- merge(res2_OSA, assoc_PRS_incident_OSA_sub, by.x=c("stratum", "PRS"))
-# 
-# res2_OSA <- rbind(res2_OSA[10:12,],res2_OSA[7:9,], res2_OSA[1:3,], res2_OSA[4:6,])
-# res2_OSA$X <- c(nrow(res2_OSA):1)
-# 
-# 
-# res2_OSA$log_IRR_p <- signif(res2_OSA$log_IRR_p, digits=1)
-# 
-# #only plot mgbPRS
-# #res2_OSA <- res2_OSA[which(res2_OSA$PRS == "mgbPRSsum"),]
-# 
-# #res2_OSA$PRS <- factor(res2_OSA$PRS, levels = c("mgbPRSsum"))
-# 
-# res2_OSA$stratum <- factor(res2_OSA$stratum, levels = c("Overall","No OSA", "Mild OSA", "Moderate to severe OSA"))
-# 
-# #only keep the columns we need
-# res2_OSA <- res2_OSA[c("X", "n", "N_case", "N_control","stratum", "PRS", "IRR", "CI_low", "CI_high", "log_IRR_p", "IRR_CI", "mean_AUC","AUC_CI_lower", "AUC_CI_upper", "AUC_CI")]
-# 
-# res2_OSA$Analysis <- "Incident"
-# 
-# res2_OSA$X <- c(4,3,2,1)
-# 
-# #rename columns of the two df's to rbind them for plotting
-# colnames(res1_OSA) <- c("X", "N", "N_case", "N_control", "stratum", "PRS", "Ratios", "CI_low", "CI_high", "log_Ratios_p", "Ratios_CI", "mean_AUC","AUC_CI_lower", "AUC_CI_upper", "AUC_CI", "Analysis")
-# colnames(res2_OSA) <- c("X", "N", "N_case", "N_control", "stratum", "PRS","Ratios", "CI_low", "CI_high", "log_Ratios_p", "Ratios_CI", "mean_AUC","AUC_CI_lower", "AUC_CI_upper", "AUC_CI", "Analysis")
-# 
-# res_OSA <- rbind(res1_OSA,res2_OSA)
-# 
-# 
-# res_OSA$Analysis <- factor(res_OSA$Analysis, levels = c("Cross-sectional","Incident"))
-# 
-# 
-# res_OSA$log_Ratios_p <- format(res_OSA$log_Ratios_p, scientific=T, digits = 1)
-# 
-# #save file with final dataset
-# write.csv(res_OSA, "/Users/yana/Library/CloudStorage/OneDrive-BethIsraelLaheyHealth/2022_diabetes_PRS_interaction_sleep/Draft/Results/raw_files/SuppFig_3a_b_c_T2D_PRS_assoc_baseline_and_incident_DM_stratified_by_OSA_severity_levels.csv")
-# 
 
-
-res_OSA <- read.csv("/Users/yana/Library/CloudStorage/OneDrive-BethIsraelLaheyHealth/2022_diabetes_PRS_interaction_sleep/Draft/Results/raw_files/SuppFig_3a_b_c_T2D_PRS_assoc_baseline_and_incident_DM_stratified_by_OSA_severity_levels.csv")
-
-
-
+res_OSA <- read.csv("/Users/yana/Documents/GitHub/Figures_repository/ForestPlots/sample_data.csv")
 
 
 generateShades <- function(baseColor) {
@@ -193,7 +45,6 @@ forest_plot_1 <- ggplot(res_OSA_mgb, aes(x = Ratios, y = X, colour = stratum)) +
   scale_x_continuous(breaks = seq(from = 0.5, to = 4.5, by = 0.5)) +
   
   theme(axis.text.y = element_blank(),
-        #axis.title.x = element_blank(),
         axis.title.y = element_blank(), 
         text = element_text(size=20),
         plot.title = element_text(size=20,hjust = 0.5))
@@ -276,4 +127,4 @@ forest_plot_w_info_1 + theme(legend.position="top")
 
 
 
-#ggsave(filename  = "/Users/yana/Library/CloudStorage/OneDrive-BethIsraelLaheyHealth/2022_diabetes_PRS_interaction_sleep/Code/Code_for_GitHub/Figures/Assoc_baseline_and_incident_DM_by_OSA_categories_forest_plot_NO_OSA_mgbPRSsum_v2.pdf", width = 18, height = 8, device='pdf', dpi=600)
+#ggsave(filename  = "Figures/Assoc_baseline_and_incident_DM_by_OSA_categories_forest_plot_NO_OSA_mgbPRSsum.pdf", width = 18, height = 8, device='pdf', dpi=600)
